02449d2d592d79804230b43182c30295d95a2a6c
Biko23 <bikojeremy@gmail.com>
2020-10-07 10:25:42 +0330

    Fixed the fibonacci loop to have 0 as the begining of the fibonacci sequence. Used an if statement in the for loop

--- fibonacciLoop.js
+++ fibonacciLoop.js
@@ -1,21 +1,27 @@
-function fibonacciLoop() {
+function fibonacciLoop(r) {
   let a = 1;
   let b = 0;
   let c;
   let d = [];
   // let sum = 0;
-  for (let i = 1; i <= 10; i++) {
-    // c = 0;
-    c = a + b;
+  for (let i = 0; i <= r; i++) {
+      if (i == 0) {
+          c = 0;
+      }
+      else{
+        // c = 0;
+        c = a + b;
+        // sum += c;
+        a = b;
+        b = c;
+        // console.log(d)
+      }
     d.push(c);
-    // sum += c;
-    a = b;
-    b = c;
-    // console.log(d)
+    
   }
-  // console.log(sum);
+  console.log(d);
   return d;
 }
-// fibonacci();
+//fibonacciLoop(2);
  
 module.exports = fibonacciLoop;
